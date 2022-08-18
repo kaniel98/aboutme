ba4ecb89cdd846c6d491bf6ffd595973ba1d570b
Kaniel <76683577+kaniel98@users.noreply.github.com>
2022-08-18 19:48:32 +0800

    Revert "Update vue.config.js"
    This reverts commit 69d9c1b62cced4305ed7a255556f8a884a3524fe.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === "production" ? "/personal-website-ts/" : "/",
-
-    transpileDependencies: true,
-});
+  transpileDependencies: true
+})
